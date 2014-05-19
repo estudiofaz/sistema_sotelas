787de3db7039db4d252d8f7085c4872697bdf22f
luisfelipe1070 <felipe@criattus.com.br>
2014-05-19 18:12:06 -0300

    cadastro de serviço quase pronto

--- public/layout/sistema/js/funcoes/servicos.js
+++ public/layout/sistema/js/funcoes/servicos.js
@@ -2,22 +2,35 @@ $(document).ready(function(){
     //inicio do autocomplete
 	var ulr = jQuery('#url').val();
 	var clientes = new Array();	
+
+	
+function verificaNumero(e) {
+    if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+        return false;
+    }
+}
+	
+function Person(label, value, id){
+
+	    
+		this.label = label;
+	    this.value = value;
+	    this.id = id;
+
+	}
 	
 	$.ajax({
         type:'JSON',
         url: ulr+'/sistema/ajax/clientes',
         
         success: function(response) {
-            
+           // alert(response);
             var felipe = eval(response);
             
-            
             for(i=0;i<felipe.length;i++){
-            	
-       		 clientes.push(felipe[i].nome);
-       		 
-       	 }
-            
+            	clientes.push(new Person(felipe[i].label, felipe[i].value, felipe[i].id));
+            }
+            //alert(clientes);
         },
         error: function(resp) {
             alert(resp.responseText);
@@ -26,7 +39,38 @@ $(document).ready(function(){
     });	
 		
 		$("#autocomplete" ).autocomplete({ 
-			source: clientes
+			source: clientes,
+			select: function (event, ui) {
+				$('#divModal').hide();
+           	  var teste = ui.item.id;
+		           	$.ajax({
+		    	        type:'JSON',
+		    	        url: ulr+'/sistema/ajax/cliente/id/'+ui.item.id,
+		    	        
+		    	        success: function(response) {
+		    	            
+		    	            var felipe = eval(response);
+		    	            
+		    	            $('#endereco').val(felipe[0].nm_endereco);
+		    	            $('#bairro').val(felipe[0].nm_bairro);
+		    	            $('#cidade').val(felipe[0].nm_cidade);
+		    	           
+		    	            $('#co_cliente').val(felipe[0].idCliente);
+		    	            $('#telefone1').val(felipe[0].telefone1);
+		    	            $('#telefone2').val(felipe[0].telefone2);
+		    	            $('#telefone3').val(felipe[0].telefone3);
+		    	            $("#estado").val(felipe[0].nm_estado);
+		    	            
+		    	        },
+		    	        error: function(resp) {
+		    	            alert(resp.responseText);
+		    	            alert('erro');
+		    	        }
+		    	    });
+            	 
+            	 
+            	  
+              } 
 			
 		
 		
@@ -34,38 +78,178 @@ $(document).ready(function(){
 
 	// fim autocomplete
 		
-		//inicio do clinte especifico
+		
+		
+		$('#autocomplete').keyup(function(){
+			 
+			 
+			$('#endereco').val('');
+            $('#bairro').val('');
+            $('#cidade').val('');
+           
+            $('#co_cliente').val('');
+            $('#telefone1').val('');
+            $('#telefone2').val('');
+            $('#telefone3').val('');
+            $("#estado").val('');
+			
+			
+		});		
+		
 	$('#autocomplete').focusout(function(){
 		 
-		 
+		 if($('#co_cliente').val() == '' ){
+			 
+			 $('#divModal').show();
+		 }
+		
+		
+		
+	});	
+	
+	$("#modal").fancybox({
+        helpers: {
+            title : {
+                type : 'float'
+            }
+        }
+    });
+		
+	
+	
+	// data orçamento
+	
+	$(".calendario").datepicker({
+	    dateFormat: 'dd/mm/yy',
+	    dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+	    dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+	    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+	    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+	    monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+	    nextText: 'Próximo',
+	    prevText: 'Anterior'
+	});
+	
+	
+	// funcionario horarios
+	
+	$('.calendario').focusout(function(){
+		
+		
+		$('#funcionario').val('');
+		$('#horario').val('');
+		$('#horario').prop('disabled', true);
+	});
+	
+	$('#funcionario').change(function(){
+		 var id = $(this).val()
+		 var data = $("#dt_orcamento").val()
+		 var array =  data.split("/");
+		 var dataCorreta = array[2]+'-'+array[1]+'-'+array[0];
+		 //alert(dataCorreta);
 		$.ajax({
 	        type:'JSON',
-	        url: ulr+'/sistema/ajax/cliente/nome/'+$(this).val(),
+	        url: ulr+'/sistema/ajax/servico/idFuncionario/'+id+'/dt_orcamento/'+dataCorreta,
 	        
 	        success: function(response) {
-	            
+	           // alert(response);
 	            var felipe = eval(response);
 	            
-	            if(felipe != ''){
-		            jQuery('#endereco').val(felipe[0].nm_endereco+" - "+felipe[0].nm_bairro);
-		            jQuery('#idAjax').val(felipe[0].idCliente);
-		            jQuery('#divModal').hide();
-	            }else{
-	            	$('#autocomplete').val('');
-	            	 jQuery('#endereco').val('');
-	            	jQuery('#divModal').show();
-	            }
-	            
+	            jQuery("#horario option").remove();
+				$('#horario').prop('disabled', false);
+				jQuery('<option/>').val(null).html('-- Horarios disponiveis --').appendTo("#hoario");
+
+				for(i = 0; i < felipe.length; i++){
+					
+					if(felipe[i] != 'ocupado'){
+					jQuery('<option/>').val(felipe[i]).html(felipe[i]).appendTo("#horario");
+					}
+					
+				}			
 	        },
 	        error: function(resp) {
 	            alert(resp.responseText);
 	            alert('erro');
 	        }
-	    });
+	    });	
+		
+	});
+	
+	
+	
+	//peças
+
+	$('#peca').change(function(){
+		
+		var idProduto = $(this).val();
+		
+		$.ajax({
+	        type:'JSON',
+	        url: ulr+'/sistema/ajax/produto/idProduto/'+idProduto,
+	        
+	        success: function(response) {
+	           // alert(response);
+	            var felipe = eval(response);
+	            
+	            	$('#valor_peca').val(felipe[0].nu_preco);
+	        },
+	        error: function(resp) {
+	            alert(resp.responseText);
+	            alert('erro');
+	        }
+	    });	
 		
 		
-	});	
 		
+	});
+	
+
+	// somente numeros
+	
+	$(".numeros").keypress(verificaNumero);
+	
+	// calculo valor total
+	
+	
+	$("#qdt_pecas").focusout(function(){
+		var valor = $("#valor_peca").val();
+		var  quantidade = $("#qdt_pecas").val();
+		var largura = $("#largura").val();
+		var altura = $('#altura').val();
+		
+		$('#valor_total').val((largura*altura)*(quantidade* valor));
+		
+	});
+	
+	$("#largura").focusout(function(){
+		var valor = $("#valor_peca").val();
+			var  quantidade = $("#qdt_pecas").val();
+			var largura = $("#largura").val();
+			var altura = $('#altura').val();
+			
+			$('#valor_total').val((largura*altura)*(quantidade* valor));
+			
+		});
+	
+	$("#altura").focusout(function(){
+		var valor = $("#valor_peca").val();
+		var quantidade = $("#qdt_pecas").val();
+		var largura = $("#largura").val();
+		var altura = $('#altura').val();
+		
+		$('#valor_total').val((largura*altura)*(quantidade* valor));
+		
+	});
+	
+	
+	// clonar peça
+	
+	$('.botao').click(function(){
+		
+		$('.clone').clone().insertAfter(this);
+		
+		
+	});
 		
 		
     
